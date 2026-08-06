54e434296aad03b303c7648ad79077be50a5fb04
Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
2026-08-06 13:59:53 +0000

    Rename the proposal

--- document/core/conf.py
+++ document/core/conf.py
@@ -66,10 +66,10 @@ editor = u'Andreas Rossberg (editor)'
 logo = 'static/webassembly.png'
 
 # The name of the GitHub repository this resides in
-repo = 'relaxed-atomics'
+repo = 'acquire-release-atomics'
 
 # The name of the proposal it represents, if any
-proposal = 'Relaxed Atomics'
+proposal = 'Acquire-Release Atomics'
 
 # The draft version string (clear out for release cuts)
 draft = ' (Draft ' + date.today().strftime("%Y-%m-%d") + ')'
